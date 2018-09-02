bb85b312d1c11b3749c26ae39eeb705be73eb0a1
RodrigoEstay <37455986+RodrigoEstay@users.noreply.github.com>
2018-09-02 20:05:00 -0300

    oof

--- Tarea1/src/tarea1/Tarea1.java
+++ Tarea1/src/tarea1/Tarea1.java
@@ -1,22 +1,38 @@
+// TODO: cuando comprador tiene mas de 20 monedas.
+//       cuando expendedor tiene mas de 50 monedas.
+//       chequear que se recibe bien el vuelto.
+//       chequear si se puede comprar con monedas de cien y 50.
+package tarea1;
 
-package problema21;
 
-
-public class Problema21 {
-    public Problema21(){
+public class Tarea1 {
+    public Tarea1(){
         
     }
     
     public static void main(String[] args) {
-        Problema21 a = new Problema21();
-        Moneda mon = new Moneda();
-        Expendedor ex = new Expendedor(2);
-        Bebida beb1 = ex.comprarBebida(mon, "cocacola");
-        Bebida beb2 = ex.comprarBebida(mon, "sprite");
-        Bebida beb3 = ex.comprarBebida(mon, "sprite");
-        System.out.println(beb1.beber());
-        System.out.println(beb2.beber());
-        System.out.println(beb3.beber());
+        Expendedor ex = new Expendedor(5,5,5);
+        Comprador com = new Comprador(ex);
+        int i;
+        Moneda mon1[];
+        Moneda mon2[];
+        Moneda mon3[];
+        mon1 = new Moneda[20];
+        mon2 = new Moneda[20];
+        mon3 = new Moneda[20];
+        for(i=0;i<20;i=i+1){
+            mon1[i] = new MCincuenta();
+            mon2[i] = new MCien();
+            mon3[i] = new MQuinientos();
+            ex.recibirMonedas(mon1[i]);
+            ex.recibirMonedas(mon2[i]);
+            ex.recibirMonedas(mon3[i]);
+        }
+        Moneda mon = new MQuinientos();
+        com.recibirMonedas(mon);
+        com.comprar("pepsi");
+        com.recibirVuelto();
+        com.beber();
     }
     
 }
@@ -25,17 +41,30 @@ class Comprador{
     private Moneda mon[];
     private Expendedor maquina;
     private Bebida bebida;
-    public Comprador(){
-        
+    private int nmon;
+    public Comprador(Expendedor ex){
+        mon = new Moneda[20];
+        nmon=0;
+        maquina = ex;
     }
-    public String recibirMonedas(Moneda mon){
-        
+    public void recibirMonedas(Moneda mon){
+        this.mon[nmon]=mon;
+        nmon=nmon+1;
     }
-    public String comprar(){
-        
+    public void comprar(String beb){
+        do{
+            nmon=nmon-1;
+            bebida=maquina.comprarBebida(mon[nmon], beb);
+            System.out.println("hola");
+            System.out.println(bebida.beber());
+        }while(bebida==null || nmon!=0);
     }
-    public String recibirVuelto(){
-    
+    public void recibirVuelto(){
+        while(true){
+            mon[nmon]=maquina.vuelto();
+            if(mon[nmon]==null) break;
+            nmon=nmon+1;
+        }
     }
     public String beber(){
         return bebida.beber();
@@ -55,7 +84,14 @@ class Expendedor{
     private int ncoca;
     private int nsprite;
     private int npepsi;
+    private int valorTotal;
     public Expendedor(int ncoca, int nsprite, int npepsi){
+        monCincuenta = new MCincuenta[50];
+        monCien = new MCien[50];
+        monQuinientos = new MQuinientos[50];
+        ncincuenta=0;
+        ncien=0;
+        nquinientos=0;
         this.ncoca=ncoca;
         this.nsprite=nsprite;
         this.npepsi=npepsi;
@@ -74,6 +110,9 @@ class Expendedor{
         }
     }
     public Bebida comprarBebida(Moneda c, String t){
+        valorTotal=valorTotal+c.obtenerValor();
+        recibirMonedas(c);
+        if(valorTotal<350) return null;
         if(t=="cocacola"){
             if(ncoca==0) return null;
             else{
@@ -92,13 +131,42 @@ class Expendedor{
             if(npepsi==0) return null;
             else{
                 npepsi=npepsi-1;
-                return sprite[npepsi];
+                return pepsi[npepsi];
             }
         }
         return null;
     }
     public void recibirMonedas(Moneda mon){
-        
+        if(mon.obtenerValor()==50){
+            monCincuenta[ncincuenta]=mon;
+            ncincuenta=ncincuenta+1;
+        }
+        else if(mon.obtenerValor()==100){
+            monCien[ncien]=mon;
+            ncien=ncien+1;
+        }
+        else if(mon.obtenerValor()==500){
+            monQuinientos[nquinientos]=mon;
+            nquinientos=nquinientos+1;
+        }
+    }
+    public Moneda vuelto(){
+        if(valorTotal>=500 && nquinientos>0){
+            valorTotal=valorTotal-500;
+            nquinientos=nquinientos-1;
+            return monQuinientos[nquinientos];
+        }
+        else if(valorTotal>=100 && ncien>0){
+            valorTotal=valorTotal-100;
+            ncien=ncien-1;
+            return monCien[ncien];
+        }
+        else if(valorTotal>=50 && ncincuenta>0){
+            valorTotal=valorTotal-50;
+            ncincuenta=ncincuenta-1;
+            return monCincuenta[ncincuenta];
+        }
+        return null;
     }
 }
 
