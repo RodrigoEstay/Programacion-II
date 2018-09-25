c448bb81d8566f6c8b8580352bb1f900d859faa0
RodrigoEstay <restay@udec.cl>
2018-09-25 17:03:15 -0300

    aa

---
+++ Tareas/Tarea 2/Tarea2/src/tarea2/Tarea2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea2;
+
+/**
+ *
+ * @author restay2016
+ */
+public class Tarea2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
